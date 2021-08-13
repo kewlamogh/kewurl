b94274b5f5deedeec02784f371d091783315d876
DaCool1 <86981743+kewlamogh@users.noreply.github.com>
2021-08-13 10:36:35 -0700

    Update kewurl.py

--- kewurl.py
+++ kewurl.py
@@ -1,1 +1,14 @@
+from flask import Flask, redirect 
+from replit import db  
+app = Flask('app')
+@app.route("/<l>") 
+def re(l):
+  return redirect(db[str(l)])
 
+@app.route("/new/<link>")
+def n(link):
+  db["n"] += 1;
+  db[str(db["n"])] = "https://"+link;
+  return f"Your shortened url is https://t.amoghthecool.repl.co/{db['n']}"
+
+app.run(host='0.0.0.0', port=8080)
