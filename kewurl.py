760da6e99e9f5c6f16ce7419a89ff78d1a6908d4
DaCool1 <86981743+kewlamogh@users.noreply.github.com>
2021-08-13 11:12:49 -0700

    Update kewurl.py

--- kewurl.py
+++ kewurl.py
@@ -1,18 +1,22 @@
-from flask import Flask, redirect 
+from flask import Flask, redirect, render_template
 from replit import db  
 try:
   db["n"]
 except:
   db["n"] = 0
-app = Flask('app')
+app = Flask('app', template_folder = "t")
 @app.route("/<l>") 
 def re(l):
   return redirect(db[str(l)])
 
 @app.route("/new/<link>")
 def n(link):
-  db["n"] += 1
-  db[str(db["n"])] = "https://"+link
-  return f"Your shortened url is https://kewl.amoghthecool.repl.co/{db['n']}"
+  db["n"] += 1;
+  db[str(db["n"])] = "https://"+link.replace("_", "/");
+  return render_template("success.html", url = f"https://t.amoghthecool.repl.co/{db['n']}")
+
+@app.route("/")
+def h():
+  return render_template("render.html")
 
 app.run(host='0.0.0.0', port=8080)
